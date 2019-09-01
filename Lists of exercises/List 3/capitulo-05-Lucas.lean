d0221905fc8e7bb4e38ff79e7b7cfe379242d65b
Lucas Emanuel Resck Domingues <41991486+lucasresck@users.noreply.github.com>
2019-08-31 21:52:06 -0300

    Almost finished

--- Lists of exercises/List 3/capitulo-05-Lucas.lean
+++ Lists of exercises/List 3/capitulo-05-Lucas.lean
@@ -1,8 +1,8 @@
-variables A B P Q R: Prop
+variables A B C P Q R: Prop
 
 open classical
 
-example (h1 : ¬ A → false) (h2 : A ∨ ¬ A) : A :=
+theorem exercise_1 (h1 : ¬ A → false) (h2 : A ∨ ¬ A) : A :=
   or.elim h2
     (assume h3 : A,
     show A, from h3)
@@ -10,7 +10,7 @@ example (h1 : ¬ A → false) (h2 : A ∨ ¬ A) : A :=
     have h5 : false, from h1 h4,
     show A, from false.elim h5)
 
-example (h1 : ¬ A ∨ ¬ B) : ¬ (A ∧ B) :=
+theorem exercise_2 (h1 : ¬ A ∨ ¬ B) : ¬ (A ∧ B) :=
   assume h2 : A ∧ B,
   have h3 : A, from and.left h2,
   have h4 : B, from and.right h2,
@@ -20,7 +20,7 @@ example (h1 : ¬ A ∨ ¬ B) : ¬ (A ∧ B) :=
     (assume h6 : ¬ B,
     show false, from h6 h4)
 
-example (h1 : ¬ (A ∧ B)) : ¬ A ∨ ¬ B :=
+theorem exercise_3 (h1 : ¬ (A ∧ B)) : ¬ A ∨ ¬ B :=
   by_contradiction
   (assume h2 : ¬ (¬ A ∨ ¬ B),
   have h4 : A, from
@@ -36,7 +36,7 @@ example (h1 : ¬ (A ∧ B)) : ¬ A ∨ ¬ B :=
   have h3 : A ∧ B, from and.intro h4 h5,
   show false, from h1 h3)
 
-example (h1 : ¬ P → (Q ∨ R)) (h2 : ¬ Q) (h3 : ¬ R) : P :=
+theorem exercise_4 (h1 : ¬ P → (Q ∨ R)) (h2 : ¬ Q) (h3 : ¬ R) : P :=
   by_contradiction
   (assume h4 : ¬ P,
   have h5 : Q ∨ R, from h1 h4,
@@ -46,7 +46,7 @@ example (h1 : ¬ P → (Q ∨ R)) (h2 : ¬ Q) (h3 : ¬ R) : P :=
     (assume h6 : R,
     show false, from h3 h6))
 
-example (h1 : A → B) : ¬ A ∨ B :=
+theorem exercise_5 (h1 : A → B) : ¬ A ∨ B :=
   by_contradiction
   (assume h2 : ¬ (¬ A ∨ B),
   have h4 : ¬ A, from
@@ -57,7 +57,7 @@ example (h1 : A → B) : ¬ A ∨ B :=
   have h3 : ¬ A ∨ B, from or.inl h4,
   show false, from h2 h3)
 
-example : A → ((A ∧ B) ∨ (A ∧ ¬ B)) :=
+theorem exercise_6 : A → ((A ∧ B) ∨ (A ∧ ¬ B)) :=
   (assume h1 : A,
   show ((A ∧ B) ∨ (A ∧ ¬ B)), from
     by_contradiction
@@ -70,3 +70,46 @@ example : A → ((A ∧ B) ∨ (A ∧ ¬ B)) :=
     have h4 : A ∧ ¬ B, from and.intro h1 h5,
     have h3 : (A ∧ B) ∨ (A ∧ ¬ B), from or.inr h4,
     show false, from h2 h3))
+
+--Exercise 8
+
+-- Prove ¬ (A ∧ B) → ¬ A ∨ ¬ B by replacing the sorry's below
+-- by proofs.
+
+lemma step1 {A B : Prop} (h₁ : ¬ (A ∧ B)) (h₂ : A) : ¬ A ∨ ¬ B :=
+have ¬ B, from 
+  (assume h₃ : B,
+  show false, from h₁ (and.intro h₂ h₃)),
+show ¬ A ∨ ¬ B, from or.inr this
+
+lemma step2 {A B : Prop} (h₁ : ¬ (A ∧ B)) (h₂ : ¬ (¬ A ∨ ¬ B)) : false :=
+have ¬ A, from
+  assume : A,
+  have ¬ A ∨ ¬ B, from step1 h₁ ‹A›,
+  show false, from h₂ this,
+show false, from h₂ (or.inl this)
+
+theorem step3 (h : ¬ (A ∧ B)) : ¬ A ∨ ¬ B :=
+by_contradiction
+  (assume h' : ¬ (¬ A ∨ ¬ B),
+    show false, from step2 h h')
+
+--Exercise 9
+
+example (h : ¬ B → ¬ A) : A → B :=
+  assume h1 : A,
+  show B, from
+    by_contradiction
+    (assume h2 : ¬ B,
+    show false, from (h h2) h1)
+
+example (h : A → B) : ¬ A ∨ B :=
+  by_contradiction
+  (assume h1 : ¬ (¬ A ∨ B),
+  have h3 : ¬ A, from
+    assume h4 : A,
+    have h6 : B, from h h4,
+    have h5 : ¬ A ∨ B, from or.inr h6,
+    show false, from h1 h5,
+  have h2 : ¬ A ∨ B, from or.inl h3,
+  show false, from h1 h2)
